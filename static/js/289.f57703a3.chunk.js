b2146b62457e5bb79c76c642c60aea5073437f52
mollyDanger <103769805+mollyDanger@users.noreply.github.com>
2024-07-14 18:50:57 -0400

    Updates

---
+++ static/js/289.f57703a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhotel_ui=self.webpackChunkhotel_ui||[]).push([[289],{3289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.f57703a3.chunk.js.map
